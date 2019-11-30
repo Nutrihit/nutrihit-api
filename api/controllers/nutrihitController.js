3441856086bd3835fcc347c5a42eb571c7bee089
Alexander Wolf <alexander@goltfisch.de>
2019-11-30 13:48:21 +0100

    Add controllers

---
+++ api/controllers/nutrihitController.js
@@ -0,0 +1,70 @@
+[
+    {
+        label: 'Day 1',
+        microNutrientsSummary: [
+            {
+                label: 'Vitamin C',
+                key: 'vitaminC',
+                value: 10.1,
+                unit: 'mcg'
+            },
+            {
+                ...
+            }
+        ],
+        food: [
+            {
+                label: 'Banana',
+                key: 'banana',
+                microNutrients: [
+                    {
+                        label: 'Vitamin C',
+                        key: 'vitaminC',
+                        value: 10.1,
+                        unit: 'mcg'
+                    },
+                    {
+                        ...
+                    }
+                ],
+            },
+            {
+                ...
+            }
+        ]
+    },
+    {
+        label: 'Day 1',
+        microNutrientsSummary: [
+            {
+                label: 'Vitamin C',
+                key: 'vitaminC',
+                value: 10.1,
+                unit: 'mcg'
+            },
+            {
+                ...
+            }
+        ],
+        food: [
+            {
+                label: 'Banana',
+                key: 'banana',
+                microNutrients: [
+                    {
+                        label: 'Vitamin C',
+                        key: 'vitaminC',
+                        value: 10.1,
+                        unit: 'mcg'
+                    },
+                    {
+                        ...
+                    }
+                ],
+            },
+            {
+                ...
+            }
+        ]
+    },
+]
--- api/controllers/profileController.js
+++ api/controllers/profileController.js
@@ -5,30 +5,35 @@ var mongoose = require('mongoose');
 exports.list_all_needed_nutrients = function(req, res) {
     var neededNutrients = [
         {
+            id: 1,
             label: 'Vitamin C',
             key: 'vitaminC',
             value: 100,
             unit: 'mg'
         },
         {
+            id: 2,
             label: 'Vitamin A',
             key: 'vitaminA',
             value: 1.1,
             unit: 'mg'
         },
         {
+            id: 3,
             label: 'Vitamin B1',
             key: 'vitaminB1',
             value: 1.3,
             unit: 'mg'
         },
         {
+            id: 4,
             label: 'Vitamin B2',
             key: 'vitaminB2',
             value: 1.5,
             unit: 'mg'
         },
         {
+            id: 5,
             label: 'Vitamin B12',
             key: 'vitaminB12',
             value: 3,
